1da00f1154eecbc346fbb782553c7464706eb589
Vunb <nhubaovu@gmail.com>
2016-09-14 10:46:02 +0700

    add new command: vntk clean [text] [-f]

--- bin/vntk.js
+++ bin/vntk.js
@@ -5,6 +5,7 @@
  */
 
 var _ = require("lodash")
+    , async = require("async")
     , program = require("commander")
     , vntk = require("../lib/vntk")
     , package = require("../package.json")
@@ -78,6 +79,63 @@ program
         console.log();
         console.log('    $ vntk segment "Chào mừng bạn đến với Việt Nam"');
         console.log('    $ vntk segment test.txt another.txt -f');
+        console.log();
+    });
+
+program
+    .command("clean [text...]")
+    .alias("cl")
+    .description("clean text from html content")
+    .option("-f, --file", "input is files")
+    .action(function (text, options) {
+        console.log('input: %s', text);
+        console.log('isFile: %s', !!options.file);
+        // console.log("typeof text: ", typeof text);
+
+        // check input
+        var input = text + "";
+        if (!input) {
+            console.log("input is required");
+            this.emit("--help");
+            return;
+        }
+
+        if (!options.file) {
+            text.forEach(function (e) {
+                console.log(e);
+                var cleaned = vntk.util.clean_html(e);
+                console.log("Output:", cleaned);
+            }, this);
+        } else {
+            var fs = require("fs");
+            async.eachSeries(text, function process(fileName, callback) {
+                fs.readFile(fileName, function read(err, data) {
+                    var outputFileName = fileName + ".cleaned";
+                    var cleaned = vntk.util.clean_html(data.toString());
+                    fs.writeFile(outputFileName, cleaned, function (err) {
+                        if (!err) {
+                            console.log("Done: ", outputFileName);
+                            callback(null);
+                        } else {
+                            console.error("Clean file error: " + fileName, err);
+                            callback(err);
+                        }
+                    });
+                });
+            }, function done(err) {
+                if (!err) {
+                    console.log("Clean text is done without error!");
+                } else {
+                    console.error("Clean text is error", err);
+                }
+            });
+        }
+
+    }).on("--help", function () {
+        console.log('  Examples:');
+        console.log();
+        console.log('    $ vntk clean "<span style="color: #4b67a1;">This is a demo</span>"');
+        console.log('    $ vntk clean test.txt another.txt -f');
         console.log();
     });
 
