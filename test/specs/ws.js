52e0d83e4f47bb8ce969c11d9d32fc5af7a68b3e
Vunb <nhubaovu@gmail.com>
2016-09-28 10:42:11 +0700

    update new testcase for vntk.ws

--- test/specs/ws.js
+++ test/specs/ws.js
@@ -1,9 +1,13 @@
-var test = require("tape");
-var vntk = require("../../lib/vntk")
+var test = require("tape")
+    , vntk = require("../../lib/vntk")
+    , ws = vntk.ws()
+    ;
 
 test("word segment text", function (t) {
-    t.plan(2);
+    t.plan(4);
 
-    t.equal(vntk.ws().segment("Thương mại và các sản phẩm cũng vậy."), "Thương_mại và các sản_phẩm cũng vậy .");
-    t.equal(vntk.ws().segment("Nhờ đó, chúng ta có thể kiềm chế căng thẳng và các xung đột tiếm năng không dẫn tới xung đột quân sự."), "Nhờ đó , chúng_ta có_thể kiềm_chế căng_thẳng và các xung_đột tiếm_năng không dẫn tới xung_đột_quân_sự .");
+    t.equal(ws.segment("Thương mại và các sản phẩm cũng vậy."), "Thương_mại và các sản_phẩm cũng vậy .");
+    t.equal(ws.segment("Nhờ đó, chúng ta có thể kiềm chế căng thẳng và các xung đột tiếm năng không dẫn tới xung đột quân sự."), "Nhờ đó , chúng_ta có_thể kiềm_chế căng_thẳng và các xung_đột tiếm_năng không dẫn tới xung_đột_quân_sự .");
+    t.equal(ws.segment("qua bộ đồ da thú"), "qua bộ_đồ da_thú");
+    t.equal(ws.segment(" ơi em đấy à "), "ơi em đấy à");
 });
