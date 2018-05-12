6758848fa36dd26cf5ce397fe1b174aa22d882fc
Vunb <nhubaovu@gmail.com>
2018-05-12 10:47:50 +0700

    add vntk api server

---
+++ server/README.md
@@ -0,0 +1,19 @@
+# NLP API Server
+
+You can easily create a NLP API Server using `Vntk`. What you need to do is decide where to deploy server. Here are some helpful ways to do this.
+
+
+## NPM Script
+
+At the project directory, run:
+
+* `$ npm start`
+
+Then open your browser or `Postman` to test apis.
+
+* http://localhost:3000/api/tok/xin%20chào%20các%20bạn
+* http://localhost:3000/api/pos/xin%20chào%20các%20bạn
+* http://localhost:3000/api/chunking/xin%20chào%20các%20bạn
+* http://localhost:3000/api/ner/xin%20chào%20các%20bạn
+
+## Docker
---
+++ server/api.js
@@ -0,0 +1,53 @@
+'use strict';
+const vntk = require('../lib/vntk');
+
+const tokenizer = vntk.wordTokenizer();
+const posTag = vntk.posTag();
+const chunking = vntk.chunking();
+const ner = vntk.ner();
+
+// kites extension definition
+module.exports = function (kites) {
+    kites.on('expressConfigure', (app) => {
+        /**
+         * Word Tokenizer
+         */
+        app.get('/api/tok/:text', (req, res) => {
+            var text = req.param('text')
+            var format = req.param('format')
+            var result = tokenizer.tag(text, format)
+            res.ok(result)
+        });
+        
+        /**
+         * POS Tagging
+         */
+        app.get('/api/pos/:text', (req, res) => {
+            var text = req.param('text')
+            var format = req.param('format')
+            var result = posTag.tag(text, format)
+            res.ok(result)
+        })
+
+        /**
+         * Chunking
+         */
+        app.get('/api/chunking/:text', (req, res) => {
+            var text = req.param('text')
+            var format = req.param('format')
+            var result = chunking.tag(text, format)
+            res.ok(result)
+        })
+
+        /**
+         * Named Entity Recognition
+         */
+        app.get('/api/ner/:text', (req, res) => {
+            var text = req.param('text')
+            var format = req.param('format')
+            var result = ner.tag(text, format)
+            res.ok(result)
+        })
+
+    })
+}
