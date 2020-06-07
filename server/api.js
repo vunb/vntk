6b28256f55336bcd2f27ea35183a6d11eb23881a
Vunb <nhubaovu@gmail.com>
2020-06-07 17:48:58 +0700

    upgrade kites server to serve vntk apis

--- server/api.js
+++ server/api.js
@@ -8,54 +8,54 @@ const ner = vntk.ner();
 
 // kites extension definition
 module.exports = function (kites) {
-    kites.on('expressConfigure', (app) => {
+  kites.on('express:config', (app) => {
 
-        /**
-         * API Homepage
-         */
-        app.get('/', (req, res) => {
-            res.send('This is an example Vntk Server!')
-        })
-
-        /**
-         * Word Tokenizer
-         */
-        app.get('/api/tok/:text', (req, res) => {
-            var text = req.param('text')
-            var format = req.param('format')
-            var result = tokenizer.tag(text, format)
-            res.ok(result)
-        });
-        
-        /**
-         * POS Tagging
-         */
-        app.get('/api/pos/:text', (req, res) => {
-            var text = req.param('text')
-            var format = req.param('format')
-            var result = posTag.tag(text, format)
-            res.ok(result)
-        })
-
-        /**
-         * Chunking
-         */
-        app.get('/api/chunking/:text', (req, res) => {
-            var text = req.param('text')
-            var format = req.param('format')
-            var result = chunking.tag(text, format)
-            res.ok(result)
-        })
-
-        /**
-         * Named Entity Recognition
-         */
-        app.get('/api/ner/:text', (req, res) => {
-            var text = req.param('text')
-            var format = req.param('format')
-            var result = ner.tag(text, format)
-            res.ok(result)
-        })
-
+    /**
+     * API Homepage
+     */
+    app.get('/', (req, res) => {
+      res.send('This is an example Vntk Server!')
     })
+
+    /**
+     * Word Tokenizer
+     */
+    app.get('/api/tok/:text', (req, res) => {
+      var text = req.param('text')
+      var format = req.param('format')
+      var result = tokenizer.tag(text, format)
+      res.ok(result)
+    });
+
+    /**
+     * POS Tagging
+     */
+    app.get('/api/pos/:text', (req, res) => {
+      var text = req.param('text')
+      var format = req.param('format')
+      var result = posTag.tag(text, format)
+      res.ok(result)
+    })
+
+    /**
+     * Chunking
+     */
+    app.get('/api/chunking/:text', (req, res) => {
+      var text = req.param('text')
+      var format = req.param('format')
+      var result = chunking.tag(text, format)
+      res.ok(result)
+    })
+
+    /**
+     * Named Entity Recognition
+     */
+    app.get('/api/ner/:text', (req, res) => {
+      var text = req.param('text')
+      var format = req.param('format')
+      var result = ner.tag(text, format)
+      res.ok(result)
+    })
+
+  })
 }
